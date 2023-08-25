eaae4597f2c3fc1a7608d91f398954283aeb9d60
Rafal_Marciniec <asd@>
2023-08-25 15:38:35 +0200

    PoC

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from collections import defaultdict
 
+import numpy as numpy
+
 path_to_file = r"data\pi.txt"
 
-
-path_to_file = r"data\pi_przyklad.txt"
+# path_to_file = r"data\pi_przyklad.txt"
 
 
 def sanitize_input(is_digits=False):
     with open(path_to_file, "r") as f:
         if is_digits:
             return [int(item.replace("\n", "")) for item in f.readlines()]
-        x = [str(item.replace("\n", "")) for item in f.readlines()]
+        raw_input = [str(item.replace("\n", "")) for item in f.readlines()]
         pairs = []
-        for idx in range(len(x) - 1):
-            pairs.append(int(x[idx] + x[idx + 1]))
+        for idx in range(len(raw_input) - 1):
+            pairs.append(int(raw_input[idx] + raw_input[idx + 1]))
         return pairs
 
 
@@ -33,29 +34,52 @@ def zad_3_2():
     a = 1
 
 
+def is_increasing(current_index, is_first_run, digits):
+    if current_index == len(digits) - 1:
+        return current_index + 1
+    if digits[current_index] >= digits[current_index + 1]:
+        if is_first_run:
+            return ""
+        return is_decreasing(current_index=current_index + 1, digits=digits)
+    return is_increasing(current_index=current_index + 1, is_first_run=False, digits=digits)
+
+
+def is_decreasing(current_index, digits):
+    if current_index == len(digits) - 1 or digits[current_index] < digits[current_index + 1]:
+        return current_index + 1
+    return is_decreasing(current_index=current_index + 1, digits=digits)
+
+
+
+
+
+
 def zad_3_3():
     digits = sanitize_input(True)
-    all_sequences = []
-    is_increasing = True
-    end_index = 0
-    start_index = 0
-    while end_index < len(digits) - 1:
-        if is_increasing:
-            if digits[end_index] < digits[end_index + 1]:
-                end_index += 1
-            else:
-                end_index += 1
-                is_increasing=False
-        else:
-            if digits[end_index] < digits[end_index + 1]:
-                all_sequences.append("".join([str(digit) for digit in digits[start_index:end_index + 1]]))
-                start_index = end_index
-                is_increasing = True
-            else:
-                end_index += 1
-    print(list(filter(lambda x: len(x)==6, all_sequences)))
-    a = 1
-    a = 1
+    all_sequences = {}
+    for main_idx in range(len(digits) - 2):
+        # if main_idx==9390:
+        end_idx = is_increasing(current_index=main_idx, is_first_run=True, digits=digits)
+        if not end_idx:
+            continue
+        all_sequences[main_idx] = ''.join(str(item) for item in digits[main_idx: end_idx])
+        a = 1
+    x = dict(filter(lambda item: len(item[1])>=6, all_sequences.items()))
+    print(x)
+    print(len(dict(filter(lambda item: len(item[1])>=6, all_sequences.items()))))
+    # print(len(dict(filter(lambda item: len(item[1])==max(all_sequences.values(), key=len), all_sequences.items()))))
+    inner_index = 1
+    inner_index = 1
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     zad_3_3()
